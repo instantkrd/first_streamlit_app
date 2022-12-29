669e94ccb8c9c0859c4a24a9250b6d8e8fdfaf81
Koteswara Rao Daliparthi <121644178+instantkrd@users.noreply.github.com>
2022-12-29 17:51:44 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit 
+import pandas
 streamlit.title('abcd')
 streamlit.header('efgh')
 streamlit.text('ijkl')
@@ -6,3 +7,5 @@ streamlit.text('mnop')
 streamlit.text('qrst')
 streamlit.header('uvwx')
 
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
