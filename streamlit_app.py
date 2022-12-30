d0e22c6f847e1d8f90307789f2fd5991bf92f781
Koteswara Rao Daliparthi <121644178+instantkrd@users.noreply.github.com>
2022-12-29 23:00:04 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,12 @@
 import streamlit 
 import pandas
-streamlit.title('abcd')
-streamlit.header('efgh')
-streamlit.text('ijkl')
-streamlit.text('mnop')
-streamlit.text('qrst')
-streamlit.header('uvwx')
+streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Favorites')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.header('Avocado Toast')
 
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
